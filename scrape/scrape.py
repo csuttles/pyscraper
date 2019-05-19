ebc6e14536a7335235ce2f41690df697e280ada2
Chris Suttles <suttles@gmail.com>
2019-05-18 22:18:34 -0500

    fix some typos and add timeout flag and retry object

--- scrape/scrape.py
+++ scrape/scrape.py
@@ -9,11 +9,11 @@ import sys
 import threading
 import time
 from queue import Queue
-
+from requests.packages.urllib3.util.retry import Retry
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-f", "--file", help="file to read (list of URLs to scrape)", action='store', default='list.txt')
-parser.add_argument("-l", "--logilfe", help="logfile to write", action='store', default='log.txt')
+parser.add_argument("-l", "--logfile", help="logfile to write", action='store', default='log.txt')
 parser.add_argument("-t", "--threads", help="number fo threads to run", action='store', default=6, type=int)
 parser.add_argument("-H", "--headers", help="headers to pass to the worker, specified in json format",
                     action='store', default='{"x-test": "ctlfish"}')
@@ -29,16 +29,30 @@ logger = logging.getLogger(__name__)
 
 def main():
     starttime = time.time()
-    logger.debug(f'invoked with {args}')
+    if args.logfile:
+        fh = logging.FileHandler(args.logfile)
+        fh.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
+        logger.addHandler(fh)
+
     if args.debug:
         logger.setLevel(logging.DEBUG)
+    logger.debug(f'invoked with {args}')
 
     data = {}
     if args.headers:
         data['headers'] = json.loads(args.headers)
     data['logger'] = logger
-    if args.timeout:
-        data['timeout'] = args.timeout
+    data['timeout'] = args.timeout
+
+
+    retry = Retry(
+        total=3,
+        read=3,
+        connect=3,
+        backoff_factor=0.3,
+        status_forcelist=None,
+    )
+    data['retry'] = retry
 
     queue = Queue()
 
@@ -65,16 +79,17 @@ class Worker(threading.Thread):
         self.queue = queue
         self.logger = data['logger']
         self.s = requests.Session()
-        adapter = requests.adapters.HTTPAdapter(max_retries=3)
+        adapter = requests.adapters.HTTPAdapter(max_retries=data['retry'])
         self.s.mount('http://', adapter)
         self.s.mount('https://', adapter)
         self.s.headers.update(data['headers'])
+        self.timeout = data['timeout']
 
     def run(self):
         while True:
             url = self.queue.get()
             try:
-                resp = self.s.get(url, headers={'X-name': f'agent {self.name}'}, timeout=5)
+                resp = self.s.get(url, headers={'X-name': f'agent {self.name}'}, timeout=self.timeout)
                 logger.info(f'{self.name} fetched {url} {resp.status_code}')
             except requests.exceptions.SSLError as err:
                 logger.error(f'{self.name} fetching {url} failed with SSL error: {err}')
@@ -82,7 +97,6 @@ class Worker(threading.Thread):
                 logger.error(f'{self.name} fetching {url} failed with Connection error: {err}')
             finally:
                 self.queue.task_done()
-        self.s.close()
 
 
 if __name__ == '__main__':
